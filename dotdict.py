3066b34621335efb517f88f9f2045079eb929013
Perry Kundert <perry@hardconsulting.com>
2013-04-04 16:40:32 -0600

    Added .. back-tracking to dotdict

--- dotdict.py
+++ dotdict.py
@@ -1,12 +1,22 @@
+
+import sys
+
 class dotdict(dict):
     """A dict supporting keys containing dots, to access a heirarchy of dicts.
-    Furthermore, if the keys form valid attribute names, also accessible by
-    dotted attribute names:
+    Furthermore, if the keys form valid attribute names, values are also
+    accessible via dotted attribute name access:
     
         >>> d = dotdict()
         >>> d["a.b"] = 1
         >>> d.a.b
         >>> 1
+
+    Every '..' in the key back-tracks by one key element (these ignored elements
+    are not checked for validity):
+
+        >>> d['a.x..b'] # same as d['a.b']
+        >>> 1
+        >>> d['a.x.y...b'] # and works for multiple levels, one dot per level
     """
     def __init__( self, value=None ):
         if value is None:
@@ -17,9 +27,26 @@ class dotdict(dict):
         else:
             raise TypeError( 'expected dict' )
 
+    def _resolve( self, key ):
+        """Return next segment in key as (one, rest), solving for any '..'
+        back-tracking.  If key begins/ends with ., or too many .. are used, the
+        key will end up prefixed by ., 'mine' will end up '', raising KeyError."""
+        mine, rest		= key, None
+        if '.' in mine:
+            while '..' in mine:
+                # 'a.b..c'     ==> 'a.c'  ; split == ['a.b',   'c'  ]
+                # 'a.b.c...d'  ==> 'a.d'  ; split == ['a.b.c', '.d' ]
+                front, back	= mine.split( '..', 1 )
+                front		= front[:front.rfind('.')]
+                mine		= front + '.' + back
+            mine, rest		= mine.split( '.', 1 )
+        if not mine:
+            raise KeyError('cannot index using key "%s"; no leading path element' % key)
+        return mine, rest
+
     def __setitem__( self, key, value ):
-        if '.' in key:
-            mine, rest          = key.split( '.', 1 )
+        mine, rest          	= self._resolve( key )
+        if rest:
             target              = dict.setdefault( self, mine, dotdict() )
             if not isinstance( target, dotdict ):
                 raise KeyError( 'cannot set "%s" in "%s" (%r)' % ( rest, mine, target ))
@@ -30,18 +57,18 @@ class dotdict(dict):
             dict.__setitem__( self, key, value )
 
     def __getitem__( self, key ):
-        if '.' not in key:
+        mine, rest              = self._resolve( key )
+        if rest is None:
             return dict.__getitem__( self, key )
-        mine, rest              = key.split( '.', 1 )
         target                  = dict.__getitem__( self, mine )
         if not isinstance( target, dotdict ):
             raise KeyError( 'cannot get "%s" in "%s" (%r)' % ( rest, mine, target ))
         return target[rest]
 
     def __contains__( self, key ):
-        if '.' not in key:
+        mine, rest              = self._resolve( key )
+        if rest is None:
             return dict.__contains__( self, key )
-        mine, rest              = key.split( '.', 1 )
         target                  = dict.__getitem__( self, mine )
         if not isinstance( target, dotdict ):
             return False
@@ -59,5 +86,37 @@ class dotdict(dict):
         except KeyError:
             return default
             
-    __setattr__ = __setitem__
-    __getattr__ = __getitem__
+    __setattr__			= __setitem__
+    __getattr__			= __getitem__
+
+    def iterkeys( self ):
+        for key,val in dict.items( self ):
+            if isinstance( val, dotdict ):
+                # A sub-dotdict; yield all its keys, prefixed with this key
+                for subkey in val.iterkeys():
+                    yield key + '.' + subkey
+            else:
+                yield key # Not a sub-dotdict; just yield the key
+
+    def itervalues( self ):
+        for key in self.iterkeys():
+            yield self[key]
+
+    def iteritems( self ):
+        for key in self.iterkeys():
+            yield key, self[key]
+
+    def __listkeys( self ):
+        return list( self.iterkeys() )
+
+    def __listvalues( self ):
+        return list( self.itervalues() )
+
+    def __listitems( self ):
+        return list( self.iteritems() )
+
+    __iter__			= iterkeys
+    keys 			= __listkeys   if sys.version_info.major < 3 else iterkeys
+    values			= __listvalues if sys.version_info.major < 3 else itervalues
+    items			= __listitems  if sys.version_info.major < 3 else iteritems
+
--- dotdict_test.py
+++ dotdict_test.py
@@ -16,7 +16,7 @@ def test_dotdict():
     d = cpppo.dotdict()
     d["a.b"] = 1
     assert d["a.b"] == 1
-    assert d.a.b == 1
+    assert d.a.b == 1		# attribute access =~= indexing
     d.a.b = 2
     assert d["a.b"] == 2
     assert d.a.b == 2
@@ -27,3 +27,32 @@ def test_dotdict():
     assert d.a.b.c.d == 2
 
     assert "b.c.d" in d.a
+
+    # Now, test paths containing back-tracking "a.b..c" ==> "a.c".  Of course,
+    # this only works with indexing, not attribute access
+    d.a.x = 3
+    assert d["a.x"] == 3
+    assert d["a.b..x"] == 3
+    assert d["a.b.c.d....x"] == 3
+    # ... but back-tracking past root doesn't work
+    try:
+        d["..a"]
+        assert False, "Should have failed trying to back-track too many levels"
+    except KeyError as e:
+        assert 'cannot index using key "..a"' in str( e ) 
+    try:
+        d["a.b...x"]
+        assert False, "Should have failed trying to back-track too many levels"
+    except KeyError as e:
+        assert 'cannot index using key "a.b...x"' in str( e ) 
+
+    # back-tracking doesn't confirm the validity of the ignored key elements:
+    assert d["a.b.c.d.e.f....d"] == 2
+
+    # key iteration
+    assert list( sorted( k for k in d )) == ['a.b.c.d', 'a.x']
+
+    # Make sure keys/items returns a list/iterator appropriate to Python version
+    import types
+    assert isinstance( d.keys(), list if sys.version_info.major < 3 else types.GeneratorType )
+    assert isinstance( d.items(), list if sys.version_info.major < 3 else types.GeneratorType )
