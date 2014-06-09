42cb5ecc6eb60244af76743dce806e13ebd76355
Perry Kundert <perry@hardconsulting.com>
2014-06-09 13:28:25 -0600

    Correct packer.io build of Debian 8 "jessie" 64-bit Vagrant box images o Update to latest Debian 8 nightly build ISO as of 2014-06-09 o Remove use of patched VMware Tools; now builds on Linux 3.14 o Actually run the packer build command!

--- packer/debian-8-amd64/scripts/vmware.sh
+++ packer/debian-8-amd64/scripts/vmware.sh
@@ -21,11 +21,6 @@ vmware-iso|vmware-ovf)
     mkdir /tmp/vmfusion-archive
     mount -o loop /home/vagrant/linux.iso /tmp/vmfusion
     tar xzf /tmp/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive
-    # For kernel 3.12+, download kgid_t/kuid_t fixes
-    if dpkg --compare-versions $(uname -r) '>=' '3.12'; then
-        echo "Downloading 3.12+ version of VMware HGFS module..."
-        wget http://box.hardconsulting.com/vmhgfs-3_12.tar -O /tmp/vmfusion-archive/vmware-tools-distrib/lib/modules/source/vmhgfs.tar
-    fi
     /tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
     umount /tmp/vmfusion
     rm -rf  /tmp/vmfusion
