5bec50b78bc072d4e15163bb1f7d1289b5c25a51
Perry Kundert <perry@hardconsulting.com>
2015-02-21 09:01:39 -0700

    Switch packer.io debian-8 jessie to try to use open-vm-tools

--- packer/debian-8-amd64/scripts/vmware.sh
+++ packer/debian-8-amd64/scripts/vmware.sh
@@ -1,36 +1,36 @@
 #!/bin/bash -eux
 
-echo "Installing VMware Tools"
+echo "Installing VMware Tools (vmhgfs) via open-vm-tools"
 
-apt-get -y install linux-headers-$(uname -r) build-essential perl
 
 case "$PACKER_BUILDER_TYPE" in 
-
-virtualbox-iso|virtualbox-ovf) 
-    mkdir /tmp/vbox
-    VER=$(cat /home/vagrant/.vbox_version)
-    mount -o loop /home/vagrant/VBoxGuestAdditions_$VER.iso /tmp/vbox 
-    sh /tmp/vbox/VBoxLinuxAdditions.run
-    umount /tmp/vbox
-    rmdir /tmp/vbox
-    rm /home/vagrant/*.iso
-    ;;
-
 vmware-iso|vmware-ovf) 
-    mkdir /tmp/vmfusion
-    mkdir /tmp/vmfusion-archive
-    mount -o loop /home/vagrant/linux.iso /tmp/vmfusion
-    tar xzf /tmp/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive
-    /tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
-    umount /tmp/vmfusion
-    rm -rf  /tmp/vmfusion
-    rm -rf  /tmp/vmfusion-archive
-    rm /home/vagrant/*.iso
     ;;
-
 *)
     echo "Unknown Packer Builder Type >>$PACKER_BUILDER_TYPE<< selected."
-    echo "Known are virtualbox-iso|virtualbox-ovf|vmware-iso|vmware-ovf."
+    echo "Known are vmware-iso|vmware-ovf."
     ;;
-
 esac
+
+packages="open-vm-tools-dkms"
+
+# If desired, set up "sid" in /etc/apt/{sources.list, preferences.d/sid_priority} This is sometimes
+# necessary to get the latest version of some package (eg. to support building on latest kernel).
+# vvvvvvvvvv Comment these lines in/out as required
+packages="open-vm-tools-dkms/sid"
+
+echo "deb http://http.us.debian.org/debian sid main" >> /etc/apt/sources.list
+cat > /etc/apt/preferences.d/sid_priority <<EOF
+Package: *
+Pin: release a=testing
+Pin-Priority: 700
+
+Package: *
+Pin: release a=unstable
+Pin-Priority: 600
+EOF
+# ^^^^^^^^^^ Enable 'sid' in sources.list
+
+
+apt-get -y update && apt-get -y install $packages
+
